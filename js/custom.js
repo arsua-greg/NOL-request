308a5f164c292f007c051dbcaf8e499e485e4b9a
arsua-greg <gregarsua@bpoc.co.jp>
2021-10-07 11:32:03 +0800

    Change: add more revisions

--- js/custom.js
+++ js/custom.js
@@ -1,79 +1,84 @@
 $('.slider').slick({
-    centerMode: true,
-    infinite: true,
-    // centerPadding: '0',
-    slidesToShow: 1.9,
-    slideWidth: 425,
-    speed: 500,
-    arrows: false,
-    dots: true,
-    variableWidth: false,
-    rtl: false,
-    responsive : [
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 1,
-          slideWidth: 425,
-          arrows: false,
-          dots: true,
-          centerMode: false,
-          infinite: true,
-          variableWidth: false,
-        }
-      },
-      {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 0.95,
-          centerMode: true,
-          centerMode: '0',
-          slideWidth: 425,
-          arrows: false,
-          dots: true,
-          centerMode: false,
-          infinite: false,
-          variableWidth: false,
-        }
-      },
-      {
-        breakpoint: 1401,
-        settings: {
-          centerMode: true,
-          infinite: true,
-          // centerPadding: '0',
-          slidesToShow: .92,
-          slideWidth: 425,
-          speed: 500,
-          arrows: false,
-          dots: true,
-          variableWidth: false,
-          rtl: false,
-        }
-      },
-      {
-        breakpoint: 1921,
-        settings: {
-          centerMode: true,
-          infinite: true,
-          // centerPadding: '0',
-          // slidesToShow: .925,
-          slidesToShow: 0.95,
-          slideWidth: 425,
-          speed: 500,
-          arrows: false,
-          dots: true,
-          variableWidth: false,
-          rtl: false,
-        }
+  centerMode: true,
+  infinite: true,
+  // centerPadding: '0',
+  slidesToShow: 1.925,
+  slideWidth: 425,
+  speed: 500,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+  variableWidth: false,
+  rtl: false,
+  responsive : [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1,
+        slideWidth: 425,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        centerMode: false,
+        infinite: true,
+        variableWidth: false,
       }
-    ]
-  });
+    },
+    {
+      breakpoint: 1025,
+      settings: {
+        centerMode: true,
+        infinite: true,
+        slidesToShow: .92,
+        slideWidth: 425,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        variableWidth: false,
+        rtl: false,
+      }
+    },
+    {
+      breakpoint: 1401,
+      settings: {
+        centerMode: true,
+        infinite: true,
+        slidesToShow: 0.968,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        variableWidth: false,
+        rtl: false,
+      }
+    },
+    {
+      breakpoint: 1701,
+      settings: {
+        centerMode: true,
+        infinite: true,
+        // centerPadding: '0',
+        // slidesToShow: .925,
+        slidesToShow: 1.88,
+        speed: 500,
+        arrows: false,
+        dots: true,
+        variableWidth: false,
+        rtl: false,
+      }
+    }
+  ]
+});
 
 
-  $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-    console.log('beforeChange', currentSlide, nextSlide);
-  });
-  $('.slider').on('afterChange', function(event, slick, currentSlide){
-    console.log('afterChange', currentSlide);
-  });
+$('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+  console.log('beforeChange', currentSlide, nextSlide);
+});
+$('.slider').on('afterChange', function(event, slick, currentSlide){
+  console.log('afterChange', currentSlide);
+});
