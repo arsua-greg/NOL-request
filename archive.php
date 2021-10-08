c5ad5ebf4884a38079705231a47b0e193a249fee
arsua-greg <gregarsua@bpoc.co.jp>
2021-10-08 18:47:57 +0800

    remove fixed width on prev and next button

--- archive.php
+++ archive.php
@@ -25,7 +25,7 @@
                 $args = array(
                     'post_type' => 'post',
                     'post_status' => 'publish',
-                    'posts_per_page' => 10,
+                    'posts_per_page' => 5,
                     'paged' => $paged,
                 );
 
