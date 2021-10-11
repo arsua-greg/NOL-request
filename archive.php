632fa5811033905ad8c2c4ed87c4b263c204aa91
arsua-greg <gregarsua@bpoc.co.jp>
2021-10-11 13:29:27 +0800

    replace archive page code for category fixing

--- archive.php
+++ archive.php
@@ -15,63 +15,49 @@
                 <?php endforeach; ?>
             <?php endif; ?>
         </div>
-        <div class="post_content--details">
-            <div class="news_content">
-                <?php
-                $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
+        <div class="news_content">
+            <div class="news_content__item">
+                <?php while (have_posts()) : the_post(); ?>
+                    <div class="news_content__item--cont">
+                        <a href="<?php echo the_permalink(); ?>"><span class="link-spanner"></span></a>
+                        <div class="title">
+                            <p class="date"><?php echo get_the_date('Y.m.d'); ?></p>
 
-                $args = array(
-                    'post_type' => 'post',
-                    'post_status' => 'publish',
-                    'posts_per_page' => 2,
-                    'paged' => $paged,
-                );
-
-                $the_query = new WP_Query($args);
-                ?>
-
-                <?php if ($the_query->have_posts()) : ?>
-                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                        <div class="news_content__item">
-                            <div class="news_content__item--cont">
-                                <a href="<?php echo the_permalink(); ?>"><span class="link-spanner"></span></a>
-                                <div class="title">
-                                    <p class="date"><?php echo get_the_date('Y.m.d'); ?></p>
-                                    <!-- GET POST TAG -->
-                                    <div class="tag_div">
-                                        <?php
-                                        $tags = get_the_tags();
-                                        if ($tags) :
-                                            foreach ($tags as $tag) : ?>
-                                                <a class="tag" href="<?php echo esc_url(get_tag_link($tag->term_id)); ?>" title="<?php echo esc_attr($tag->name); ?>"><?php echo esc_html($tag->name); ?></a>
-                                            <?php endforeach; ?>
-                                        <?php endif; ?>
-                                    </div>
-                                    <!-- END POST TAG -->
-                                    <!-- GET CATEGORY NAME -->
-                                    <?php foreach ((get_the_category()) as $cat) {
-                                        echo $sep . '<a href="' . get_category_link($cat->term_id) . '"  class="' . $cat->slug . '" ' . esc_attr($cat->name) . '">' . $cat->cat_name . '</a>';
-                                    } ?>
-                                    <!-- END GET CATEGORY -->
-                                </div>
-                                <p class="text">
-                                    <?php the_title(); ?>
-                                </p>
-                                <div class="permalink">
-                                    <a href="<?php echo get_permalink(); ?>">
-                                        <p>detail</p>
-                                        <img src="<?php echo get_template_directory_uri(); ?>/img/common/arrow_right_b.svg" alt="">
-                                    </a>
-                                </div>
+                            <!-- GET POST TAG -->
+                            <div class="tag_div">
+                                <?php
+                                $tags = get_the_tags();
+                                if ($tags) :
+                                    foreach ($tags as $tag) : ?>
+                                        <a class="tag" href="<?php echo esc_url(get_tag_link($tag->term_id)); ?>" title="<?php echo esc_attr($tag->name); ?>"><?php echo esc_html($tag->name); ?></a>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
                             </div>
+                            <!-- END POST TAG -->
+
+                            <!-- GET CATEGORY NAME -->
+                            <?php foreach ((get_the_category()) as $cat) {
+                                echo $sep . '<a href="' . get_category_link($cat->term_id) . '"  class="' . $cat->slug . '" ' . esc_attr($cat->name) . '">' . $cat->cat_name . '</a>';
+                            } ?>
+                            <!-- END GET CATEGORY -->
                         </div>
-                    <?php endwhile; ?>
-                    <?php wp_reset_postdata(); ?>
-                <?php endif; ?>
+                        <p class="text">
+                            <?php the_title(); ?>
+                        </p>
+                        <div class="permalink">
+                            <a href="<?php echo get_permalink(); ?>">
+                                <p>detail</p>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/common/arrow_right_b.svg" alt="">
+                            </a>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
                 <div class="pagination">
-                    <?php echo easy_wp_pagenavigation($the_query); ?>
+                    <?php echo easy_wp_pagenavigation(); ?>
                 </div>
             </div>
         </div>
     </div>
-    <?php get_footer(); ?>
+</div>
+<?php get_footer(); ?>
